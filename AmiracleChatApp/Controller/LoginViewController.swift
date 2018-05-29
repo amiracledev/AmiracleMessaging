9b6bbebfea375a4e3a8c2dd71dff01e1f6a5ff14
Amir Nickroo <amiracledev@gmail.com>
2018-05-29 01:29:44 -0700

    Login and Firebase Integration

--- AmiracleChatApp/Controller/LoginViewController.swift
+++ AmiracleChatApp/Controller/LoginViewController.swift
@@ -26,6 +26,7 @@ class LoginViewController: UIViewController {
         button.translatesAutoresizingMaskIntoConstraints = false
         button.setTitleColor(UIColor.white, for: .normal)
         button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        button.addTarget(self, action: <#T##Selector#>, for: )
         return button
     }()
     
@@ -41,16 +42,54 @@ class LoginViewController: UIViewController {
         v.backgroundColor = UIColor(red: 220/255, green: 220/255, blue: 220/255, alpha: 1)
         return v
     }()
+    
+    let emailTextField: UITextField = {
+        let tf = UITextField()
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        tf.placeholder = "Email"
+        return tf
+    }()
+    let emailSeperator: UIView = {
+        let v = UIView()
+        v.translatesAutoresizingMaskIntoConstraints = false
+        v.backgroundColor = UIColor(red: 220/255, green: 220/255, blue: 220/255, alpha: 1)
+        return v
+    }()
+    let passwordTextField: UITextField = {
+        let tf = UITextField()
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        tf.placeholder = "Password"
+        tf.isSecureTextEntry = true
+        return tf
+    }()
+    
+    let profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "iOSicon")
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = UIColor(red: 61/255, green: 91/255, blue: 151/255, alpha: 1)
+        view.backgroundColor = UIColor(red: 225/255, green: 225/255, blue: 225/255, alpha: 1)
         view.addSubview(inputsContainerView)
         view.addSubview(loginRegisterButton)
+        view.addSubview(profileImageView)
         setupInputsContainer()
         setupLoginRegister()
+        setupProfileImageView()
     }
     
+    func setupProfileImageView() {
+        profileImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        profileImageView.bottomAnchor.constraint(equalTo: inputsContainerView.topAnchor, constant: -50).isActive = true
+        profileImageView.widthAnchor.constraint(equalToConstant: 150).isActive = true
+        profileImageView.heightAnchor.constraint(equalToConstant: 150).isActive = true
+        
+        
+    }
     func setupInputsContainer() {
         inputsContainerView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         inputsContainerView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
@@ -64,11 +103,32 @@ class LoginViewController: UIViewController {
         nameTextField.heightAnchor.constraint(equalTo: inputsContainerView.heightAnchor, multiplier: 1/3).isActive = true
         
         inputsContainerView.addSubview(nameSeperator)
+        inputsContainerView.addSubview(emailTextField)
+        inputsContainerView.addSubview(emailSeperator)
+        inputsContainerView.addSubview(passwordTextField)
+    
         
         nameSeperator.leftAnchor.constraint(equalTo: inputsContainerView.leftAnchor).isActive = true
         nameSeperator.topAnchor.constraint(equalTo: nameTextField.bottomAnchor).isActive = true
         nameSeperator.widthAnchor.constraint(equalTo: inputsContainerView.widthAnchor).isActive = true
         nameSeperator.heightAnchor.constraint(equalToConstant: 1).isActive = true
+        
+        
+        emailTextField.leftAnchor.constraint(equalTo: inputsContainerView.leftAnchor, constant: 12).isActive = true
+        emailTextField.topAnchor.constraint(equalTo: nameTextField.bottomAnchor).isActive = true
+        emailTextField.widthAnchor.constraint(equalTo: inputsContainerView.widthAnchor).isActive = true
+        emailTextField.heightAnchor.constraint(equalTo: inputsContainerView.heightAnchor, multiplier: 1/3).isActive = true
+        
+        emailSeperator.leftAnchor.constraint(equalTo: inputsContainerView.leftAnchor).isActive = true
+        emailSeperator.topAnchor.constraint(equalTo: emailTextField.bottomAnchor).isActive = true
+        emailSeperator.widthAnchor.constraint(equalTo: inputsContainerView.widthAnchor).isActive = true
+        emailSeperator.heightAnchor.constraint(equalToConstant: 1).isActive = true
+        
+        passwordTextField.leftAnchor.constraint(equalTo: inputsContainerView.leftAnchor, constant: 12).isActive = true
+        passwordTextField.topAnchor.constraint(equalTo: emailTextField.bottomAnchor).isActive = true
+        passwordTextField.widthAnchor.constraint(equalTo: inputsContainerView.widthAnchor).isActive = true
+        passwordTextField.heightAnchor.constraint(equalTo: inputsContainerView.heightAnchor, multiplier: 1/3).isActive = true
+        
     }
     func setupLoginRegister() {
         loginRegisterButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
--- AmiracleChatApp/Controller/ViewController.swift
+++ AmiracleChatApp/Controller/ViewController.swift
@@ -7,11 +7,14 @@
 //
 
 import UIKit
-
+import FirebaseAuth
+import FirebaseDatabase
 class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let ref = Database.database().reference(fromURL: "https://amiraclemessaging.firebaseio.com/")
+        ref.updateChildValues(["somevalue": 123123])
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "LogOut", style: .plain, target: self, action: #selector(handleLogout))
     }
 
